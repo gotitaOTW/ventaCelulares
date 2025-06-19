6c03792ab602dca148c1b61076e1a9821c0cfa7b
THIAGO MARTIN GARCIA SALMASO <48112397@ort>
2025-06-19 12:36:25 -0300

    a

---
+++ src/components/MostrarCatalogo.jsx
@@ -0,0 +1,22 @@
+const MostrarCatalogo = (props) =>{
+    return(
+        <>
+        <div className="catalogo">
+            {props.catalogo.map((celu,index)=>(
+                <div className="cardCelu" hey={index}>
+                    <img src={celu.fotos[0]} alt={celu.fotos[0]} className="fotoCelu"></img>
+                    <div className="datos">
+                        <div className="principales">
+                            <p className="titulo">{celu.modelo}</p>
+                            <p className="precio">{celu.precio}</p>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+            
+        </>
+    )
+}
+
+export default MostrarCatalogo;
--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,10 +1,33 @@
 import React, { useEffect, useState } from "react"
+import {celulares} from '../data/data.js';
+import {marcas} from '../data/data.js';
+import lupa from '../assets/icons/lupa.png';
+import MostrarCatalogo from "../components/MostrarCatalogo.jsx";
 
 const Catalogo = () =>{
     
     return(
         <>
-        <h1>catalogito</h1>
+        <div className="buscadores">
+            <div className="filtros">
+                <button className="filtroActivo" type="button" onClick="">Apple</button>
+                <button className="filtroActivo" type="button" onClick="">Samsung</button>
+                <button className="filtroActivo" type="button" onClick="">Xiaomi</button>
+            </div> 
+
+            <form className="formBuscarCelular">
+                <input type="text" className="inputBuscarCelular"/>
+                <button type="submit" className="submitBuscarCelular">
+                    <img src={lupa} className="imagenSubmitBuscarCelular"/>
+                </button>  
+            </form>
+        </div>
+
+        <div className="cont-catalogo">
+           <MostrarCatalogo catalogo={celulares}/> 
+        </div>
+        
+        
         </>
     )
 }
